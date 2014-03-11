7c0e3f2bc5540bf17131fcb02d9a0e0deca9df97
Oliver <bowman@knights.ucf.edu>
2014-03-11 13:29:04 -0400

    Various install fixes.

--- TEA_config.py
+++ TEA_config.py
@@ -13,11 +13,11 @@ exp       =  40  # (Def: 40) Decimal digit to limit mole fraction
 
 # ### Change below to control output files / displays
 doprint = False # (Def: False) Enable various debug printouts 
-times   = False # (Def: False) Enable time printing for speed tests 
+times   = True # (Def: False) Enable time printing for speed tests 
 
 save_headers = False # (Def: False) Preserve headers for multi-TP 
                     #   pre-atm files 
-save_outputs = True # (Def: False) Preserve intermediate outputs for 
+save_outputs = False # (Def: False) Preserve intermediate outputs for 
                     #   multi-TP pre-atm files 
 clean        = True # (Def: True) Erase any intermediate files made 
                     #   after production
